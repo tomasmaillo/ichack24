78c7c5c4b648df54182427060ab2c19d816a7285
tomas <tomasroma64@gmail.com>
2024-02-04 09:27:32 +0000

    we pray

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ ReactDOM.render(
       justifyContent: 'center',
       backgroundColor: '#F4F4F4',
     }}>
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router basename={'/ichack24'}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/student" element={<Student />} />
